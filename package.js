a4401f0301fe7abf0728a35a2645b4f8d9808a8b
Becool <shiyueming@dorasdream.com>
2016-06-30 17:57:15 +0800

    change git repository to "leaf4monkey".

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
     name: 'leaf4monkey:raw-collection',
-    version: '0.0.2',
+    version: '0.0.3',
     // Brief, one-line summary of the package.
     summary: 'expose some methods from mongo driver.',
     // URL to the Git repository containing the source code for this package.
-    git: 'https://github.com/meteor-packages/raw-collection.git',
+    git: 'https://github.com/leaf4monkey/raw-collection.git',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
