3cdfa1267128bb3927f5ec827a0a82a49657c980
Becool <shiyueming@dorasdream.com>
2016-06-30 17:39:31 +0800

    add git url.

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
     name: 'leaf4monkey:raw-collection',
-    version: '0.0.1',
+    version: '0.0.2',
     // Brief, one-line summary of the package.
     summary: 'expose some methods from mongo driver.',
     // URL to the Git repository containing the source code for this package.
-    git: '',
+    git: 'https://github.com/meteor-packages/raw-collection.git',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
